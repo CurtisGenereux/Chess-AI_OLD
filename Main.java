06d34b9d0d7365543360b48e4bcf2aa598f44473
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-08 19:23:31 -0600

    centered board and adjusted for user resolution

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class Main {
 	
@@ -12,13 +12,32 @@ public class Main {
 		Color baishe = new Color (241, 216, 179); 
 		Color brown = new Color (169, 129, 97);
 		
+		// grab user resolution
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		
+		int tileSize;
+		
+		if (width < 1920 || height < 1080) {
+			tileSize = 64;
+		} else {
+			tileSize = 128; // >1080p => 128px;
+		}
+		
 		JFrame frame = new JFrame();
 		frame.setBounds(10, 10, 512, 512);
+        frame.setLocationRelativeTo(null);
 		JPanel panel = new JPanel() {
+			
 			@Override
 			public void paint(Graphics g) {
 				
-				boolean white = true;
+				boolean white = true; // or baishe
+				
+                int boardSize = tileSize * 8;
+                int startX = (getWidth() - boardSize) / 2;
+                int startY = (getHeight() - boardSize) / 2;
 				
 				for (int i = 0; i < 8; i++) {
 					for (int j = 0; j < 8; j++) {
@@ -28,7 +47,7 @@ public class Main {
 							g.setColor(brown);
 						}
 						
-						g.fillRect(j*108, i*108, 108, 108);
+						g.fillRect(startX + j * tileSize, startY + i * tileSize, tileSize, tileSize);
 						white=!white; // if (white == false) => true; (else) => true;
 					}
 					white=!white;  // alternate for checker pattern;
