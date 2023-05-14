63c936cbe100cc387a69dfc29d3368c8d4a676ea
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-13 21:35:34 -0600

    new pieces now added to the list

--- src/Main.java
+++ src/Main.java
@@ -57,44 +57,55 @@ public class Main {
 		// board should be 80% of the screen
 		int tileSize = (int) Math.round(height*0.1);
 		
-		// define piece lsit and board;
+		// define piece list and board;
 		LinkedList<Piece> pieces = new LinkedList<>();
 		Piece[][] board = new Piece[8][8];
 		
 		ImageLoader imageLoader = new ImageLoader();
 		images = new Image[14];
+		
+		// load piece images
+		String[] imageNames = {"pawn", "knight-left", "knight-right", "rook", "bishop", "queen", "king"};
+		
+		for (int i = 0; i < 7; i++) {
+			images[i] = imageLoader.loadImage("black-" + imageNames[i], tileSize);
+		} int imageIndex = 0;
+		for (int i = 7; i < 14; i++) {
+			images[i] = imageLoader.loadImage("white-" + imageNames[imageIndex], tileSize);
+			imageIndex++;
+		}
 	
 		// asign pieces
 		for (int i = 0; i < 8; i++) {
-		    Piece blackPawn = new Piece(i, 1, "black-pawn", pieces);
-		    Piece whitePawn = new Piece(i, 6, "white-pawn", pieces);
-		    board[i][1] = blackPawn;
-		    board[i][6] = whitePawn;
+		    Piece whitePiece = new Piece(i, 1, "white-pawn", pieces);
+		    Piece blackPiece = new Piece(i, 6, "black-pawn", pieces);
+		    pieces.add(whitePiece);
+		    pieces.add(blackPiece);
+		    board[i][6] = whitePiece;
+		    board[i][1] = blackPiece;
 		}
-		
+
 		String[] pieceNames = {"rook", "knight-right", "bishop", "queen", "king", "bishop", "knight-left", "rook"};
 		
 		for (int i = 0; i < 8; i++) {
-		    Piece blackPiece = new Piece(i, 0, "black-" + pieceNames[i], pieces);
-		    Piece whitePiece = new Piece(i, 7, "white-" + pieceNames[i], pieces);
-		    board[i][0] = blackPiece;
+		    Piece whitePiece = new Piece(i, 0, "white-" + pieceNames[i], pieces);
+		    Piece blackPiece = new Piece(i, 7, "black-" + pieceNames[i], pieces);
+		    pieces.add(whitePiece);
+		    pieces.add(blackPiece);
 		    board[i][7] = whitePiece;
+		    board[i][0] = blackPiece;
 		}
 		
-		// load piece images
-		String[] uniquePieceNames = {"pawn", "knight-left", "knight-right", "rook", "bishop", "queen", "king"};
+		// move test
+		for (Piece piece : pieces) {
+		    if (piece.name.equals("white-pawn")) {
+		        piece.move(4, 7);
+		        break;
+		    }
+		}
 		
-		for (int i = 0; i < 7; i++) {
-			images[i] = imageLoader.loadImage("black-" + uniquePieceNames[i], tileSize);
-		}
-		int imageIndex = 0;
-		for (int i = 7; i < 14; i++) {
-			images[i] = imageLoader.loadImage("white-" + uniquePieceNames[imageIndex], tileSize);
-			imageIndex++;
-		}
-
-		Color baishe = new Color (241, 216, 179); // -50, -0, -25;
-		Color brown = new Color (169, 129, 97); // - 50, -0, -25;
+		Color baishe = new Color (241, 216, 179); //legal: -50, -0, -25; //current: -0, -125,  -75;
+		Color brown = new Color (169, 129, 97); //legal: - 50, -0, -25;
 		
 		JFrame frame = new JFrame();
 		frame.setSize(tileSize*8+16, tileSize*8+38); // y axis is larger because of tab
@@ -122,13 +133,13 @@ public class Main {
 						g.fillRect(startX + y * tileSize, startY + x * tileSize, tileSize, tileSize);
 					}
 				}
-
+				
 				for (Piece piece: pieces) {
-
+					
 					int Xpiece = startX + piece.xPosition * tileSize;
 					int Ypiece = startY + piece.yPosition * tileSize;
-
-					g.drawImage(assignPieces(piece), Xpiece, Ypiece, this);
+			        
+			        g.drawImage(assignPieces(piece), Xpiece, Ypiece, this);
 				}
 			}
 		};
