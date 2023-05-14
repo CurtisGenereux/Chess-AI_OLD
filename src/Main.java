1e2e5b3b5cb082c7629f51f7fcddd96ef1a279b8
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-14 08:52:55 -0600

    added mouse listener that prints tile index unmaximized

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.LinkedList;
 
 public class Main {
@@ -77,19 +80,19 @@ public class Main {
 	
 		// asign pieces
 		for (int i = 0; i < 8; i++) {
-		    Piece whitePiece = new Piece(i, 1, "white-pawn", pieces);
-		    Piece blackPiece = new Piece(i, 6, "black-pawn", pieces);
-		    pieces.add(whitePiece);
-		    pieces.add(blackPiece);
-		    board[i][6] = whitePiece;
-		    board[i][1] = blackPiece;
+		    Pawn whitePawn = new Pawn(i, 1, "white-pawn", pieces);
+		    Pawn blackPawn = new Pawn(i, 6, "black-pawn", pieces);
+		    pieces.add(whitePawn);
+		    pieces.add(blackPawn);
+		    board[i][6] = whitePawn;
+		    board[i][1] = blackPawn;
 		}
 
 		String[] pieceNames = {"rook", "knight-right", "bishop", "queen", "king", "bishop", "knight-left", "rook"};
-		
+
 		for (int i = 0; i < 8; i++) {
-		    Piece whitePiece = new Piece(i, 0, "white-" + pieceNames[i], pieces);
-		    Piece blackPiece = new Piece(i, 7, "black-" + pieceNames[i], pieces);
+			Piece whitePiece = new Piece(i, 0, "white-" + pieceNames[i], pieces);
+			Piece blackPiece = new Piece(i, 7, "black-" + pieceNames[i], pieces);
 		    pieces.add(whitePiece);
 		    pieces.add(blackPiece);
 		    board[i][7] = whitePiece;
@@ -98,7 +101,7 @@ public class Main {
 		
 		// move test
 		for (Piece piece : pieces) {
-		    if (piece.name.equals("white-pawn")) {
+		    if (piece instanceof Pawn) {
 		        piece.move(4, 7);
 		        break;
 		    }
@@ -109,20 +112,23 @@ public class Main {
 		
 		JFrame frame = new JFrame();
 		frame.setSize(tileSize*8+16, tileSize*8+38); // y axis is larger because of tab
-        	frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null);
         
 		JPanel panel = new JPanel() {
-		
+			
+			int width = 0;
+			
+			
 			public void paint(Graphics g) {
 				
 				Color backgroundColor = new Color(255, 251, 240);
-				g.setColor(backgroundColor);
-				g.fillRect(0, 0, width, height);
-
+		        g.setColor(backgroundColor);
+		        g.fillRect(0, 0, width, height);
+				
 				int boardSize = tileSize * 8;
 				int startX = (getWidth() - boardSize) / 2;
 				int startY = (getHeight() - boardSize) / 2;
-
+				
 				for (int x = 0; x < 8; x++) {
 					for (int y = 0; y < 8; y++) {
 						if (((x + y) % 2) != 0) {
@@ -137,13 +143,65 @@ public class Main {
 				for (Piece piece: pieces) {
 					
 					int Xpiece = startX + piece.xPosition * tileSize;
-					int Ypiece = startY + piece.yPosition * tileSize;
+			        int Ypiece = startY + piece.yPosition * tileSize;
 			        
 			        g.drawImage(assignPieces(piece), Xpiece, Ypiece, this);
 				}
+
 			}
+			
 		};
 		frame.add(panel);
 		frame.setVisible(true);
+		panel.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				int mouseXPos = e.getX();
+				int mouseYPos = e.getY();
+				
+				int pieceXIndex = Math.round(mouseXPos/tileSize);
+				int pieceYIndex = Math.round(mouseYPos/tileSize);
+
+				System.out.println("picked up piece at: [" + pieceXIndex + ", " + pieceYIndex + "]");
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				
+				int mouseXPos = e.getX();
+				int mouseYPos = e.getY();
+				
+				int pieceXIndex = Math.round(mouseXPos/tileSize);
+				int pieceYIndex = Math.round(mouseYPos/tileSize);
+				
+				System.out.println("dropped up piece at: [" + pieceXIndex + ", " + pieceYIndex + "]");
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) { }
+			@Override
+			public void mouseEntered(MouseEvent e) { }
+			@Override
+			public void mouseExited(MouseEvent e) { }
+		});
+		panel.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+		});
 	}
 }
