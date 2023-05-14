dab2daf35251ce54cba6f256027e9ef682d256eb
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-14 08:51:11 -0600

    created pawn class

---
+++ src/Pawn.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.LinkedList;
+
+public class Pawn extends Piece {
+
+	public Pawn(int xPosition, int yPosition, String name, LinkedList<Piece> pieces) {
+		super(xPosition, yPosition, name, pieces);
+		// TODO Auto-generated constructor stub
+	}
+
+}
