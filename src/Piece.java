218372b75a0b1522ee806cdb73cd6dd516d95564
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-14 18:46:07 -0600

    added some logic to remove team killing

--- src/Piece.java
+++ src/Piece.java
@@ -9,36 +9,45 @@ public class Piece {
 	int yPosition;
 	LinkedList<Piece> pieces;
 	String name;
+	boolean isLightPiece;
 	
-	public Piece(int xPosition, int yPosition, String name, LinkedList<Piece> pieces) {
+	public Piece(int xPosition, int yPosition, String name, boolean isLightPiece, LinkedList<Piece> pieces) {
 		
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
 		this.pieces = pieces;
+		this.isLightPiece = isLightPiece;
 		this.name = name;
 		pieces.add(this);
 		
 	}
 	
-	public void move(int xPosition, int yPosition) {
+	public void move(int xPosition, int yPosition, boolean isLightPiece) {
+		
+		int oldX = xPosition;
+		int oldY = yPosition;
+		
+		this.isLightPiece = isLightPiece;
 		
 		Iterator<Piece> pieceList = pieces.iterator();
 		Piece piece;
 		
 		while (pieceList.hasNext() == true) {
 			piece = pieceList.next();
-			if ((piece.xPosition == xPosition) && (piece.yPosition == yPosition)) {
-				pieceList.remove(); 
+			if (piece != null) {
+				this.xPosition = xPosition;
+				this.yPosition = yPosition;
 			}
+			if ((piece.xPosition == xPosition) && (piece.yPosition == yPosition) && (piece != null) && (piece.isLightPiece != isLightPiece)) {
+				pieceList.remove(); 
+				this.xPosition = xPosition;
+				this.yPosition = yPosition;
+			} else {
+				// snap piece back to start tile
+			}
+			
 		}
-		
-		this.xPosition = xPosition;
-		this.yPosition = yPosition;
 		
 	}
 	
-	public void kill() {
-		pieces.remove(this);
-		
-	}
 }
