d262040aa4b5be1382f3dce2a8ebcd46dda3f884
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-14 18:45:08 -0600

    created king class

---
+++ src/King.java
@@ -0,0 +1,14 @@
+package chess;
+
+import java.util.LinkedList;
+
+public class King extends Piece{
+
+	public King(int xPosition, int yPosition, String name, boolean isLightPiece, LinkedList<Piece> pieces) {
+		super(xPosition, yPosition, name, isLightPiece, pieces);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
