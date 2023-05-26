10c68aa741df9b22012d5ce7a09e83a792b41aba
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-26 12:13:55 -0600

    seperating isLegalmove() from move()

--- ChessAI/src/chess/Piece.java
+++ ChessAI/src/chess/Piece.java
@@ -39,7 +39,51 @@ public class Piece {
 	
 	public void move(int xPosition, int yPosition, boolean isLightPiece) {
 		
+		isMoveLegal = isBaseMoveLegal();
+		System.out.println(isMoveLegal);
+		
 		Piece targetPiece = Main.getPiece(xPosition, yPosition);
+		
+		//bounds or your color => move back; illegal;
+		//piece there => move + kill; legal;
+		//piece not there => move; legal;
+		
+		if (!isMoveLegal) {
+			return;
+		}
+		
+		if ((targetPiece.xPosition == xPosition) && (targetPiece.yPosition == yPosition)) {
+			pieces.remove(targetPiece);
+			this.xPosition = xPosition;
+			this.yPosition = yPosition;
+		} else {
+			this.xPosition = xPosition;
+			this.yPosition = yPosition;
+		}
+		
+		
+		/*
+		if ((targetPiece.xPosition == xPosition) && (targetPiece.yPosition == yPosition)) {
+			pieces.remove(targetPiece);
+			this.xPosition = xPosition;
+			this.yPosition = yPosition;
+			return;
+		
+		} else {
+			this.xPosition = xPosition;
+			this.yPosition = yPosition;
+		}
+		
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+		
+		
+		
+		if ((xPosition >= 0 && yPosition >= 0) && (xPosition <= 7 && yPosition <= 7)) {
+			this.originalX = this.xPosition;
+			this.originalY = this.yPosition;
+			isMoveLegal = true;
+		}	
 		
 		if (targetPiece == null) {
 			return;
@@ -57,12 +101,43 @@ public class Piece {
 			this.xPosition = xPosition;
 			this.yPosition = yPosition;
 		}
+
+		*/
+	
+	}
+	
+	public boolean isBaseMoveLegal() {
 		
-		if ((xPosition >= 0 && yPosition >= 0) && (xPosition <= 7 && yPosition <= 7)) {
-			this.originalX = this.xPosition;
-			this.originalY = this.yPosition;
-			isMoveLegal = true;
-		}	
+		Piece targetPiece = Main.getPiece(xPosition, yPosition);
+		
+		if (targetPiece == null) {
+			if (xPosition < 0 || yPosition < 0 || xPosition > 7 || yPosition > 7) {
+				System.out.println("false + bounds");
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		if ((targetPiece.xPosition != xPosition) || (targetPiece.yPosition != yPosition)) {
+			if (xPosition < 0 || yPosition < 0 || xPosition > 7 || yPosition > 7) {
+				return false;
+			}
+		}
+		
+		if ((targetPiece.xPosition == xPosition) && (targetPiece.yPosition == yPosition)) {
+			if ((targetPiece.isLightPiece != isLightPiece)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	public void checkIfMoveLegal() {
 		
 	}
 	
