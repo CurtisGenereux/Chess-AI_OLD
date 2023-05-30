f6fb981885f7feefb6eebcf81e1c53072d0ab489
Curtis Genereux <92477925+CurtisGenereux@users.noreply.github.com>
2023-05-30 08:44:36 -0600

    working on unique moves

--- ChessAI/src/pieces/Pawn.java
+++ ChessAI/src/pieces/Pawn.java
@@ -2,16 +2,29 @@ package pieces;
 
 import java.util.LinkedList;
 
-import chess.Piece;
-
 public class Pawn extends Piece {
-
+	
 	public Pawn(int xPosition, int yPosition, String name, boolean isLightPiece, LinkedList<Piece> pieces) {
 		super(xPosition, yPosition, name, isLightPiece, pieces);
-		// TODO Auto-generated constructor stub
 	}
+	
+	public boolean checkLegalUniqueMoves() {
+		
+		System.out.println(yPosition);
+		System.out.println(isLightPiece);
+		
+		if (isLightPiece && yPosition >= 5) {
+			
+			System.out.println("here");
+			return true;
 
-
-
+			
+		} else if (!isLightPiece && yPosition <= 2) {
+			return true;
+		}
+	
+		return false;
+		
+	}
 
 }
